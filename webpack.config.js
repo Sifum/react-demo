2e2664219cfffc7d575acacb1067021f854e4800
pocanda <472799073@qq.com>
2017-08-13 22:36:24 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = {
+    entry: [
+      "./src/entry.js"
+    ],
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: "bundle.js"
+    },
+    module: {
+      loaders: [
+        { test: /\.js[x]?$/, loader: "babel-loader?presets[]=es2015&presets[]=react", include: /src/},
+        { test: /\.css$/, loader: "style!css"},
+        { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader"},
+        { test: /\.(png|jpg)$/, loader: 'url?limit=8192'}
+      ]
+    }
+  }
