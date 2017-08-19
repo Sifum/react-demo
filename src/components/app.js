2281968e0cd11e1a575a482b07618ea2d94217d9
pocanda <472799073@qq.com>
2017-08-19 10:14:26 +0800

    changed

--- src/components/app.js
+++ src/components/app.js
@@ -1,1 +1,33 @@
-alert('success');
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Counter extends React.Component { //定义组件，继承父类
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+
+        this.state = {
+            count: 0,
+        }
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+    render() {
+        return (<div>
+                <p>{this.state.count}</p>
+                <a href="#" onClick={this.handleClick}>更新</a>
+            </div>)
+    }
+
+}
+
+
+ReactDOM.render(<Counter/>,document.getElementById('Counter'))
